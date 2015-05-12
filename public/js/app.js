9d456919f810841643639e3bac1ce27cf695c276
Cory Westropp <cory.r.west@gmail.com>
2015-05-12 11:48:26 -0700

    Worked on first four pages. Added .idea for enviroment settings.

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,65 @@ app.controller('introCtrl', ['$scope', function ($scope) {
 
 }]);
 
-app.controller('theNameCtrl', ['$scope', function ($scope) {
+app.controller('theNameCtrl', ['$scope', '$window', function ($scope, $window) {
+    $scope.model = {
+        keywords: ''
+    };
 
+    $scope.searchDomain = function() {
+        // Build the url for domain suggestions
+        var url = 'http://www.domainsbot.com/d/' + $scope.model.keywords;
+
+        var confirm = window.confirm('You will be redirected to a domain suggestion site. ' +
+            'Once you have picked a domain please return here to continue.');
+        if(confirm) {
+            $window.open(url);
+        }
+    }
+}]);
+
+app.controller('hostingCtrl', ['$scope', '$window', function ($scope, $window) {
+    $scope.model = {
+        hostingPurchased: false
+    };
+
+    $scope.buyHosting = function() {
+        var url = 'https://checkout.hostgator.com/signup/shared/29/24/websiteinaweekend';
+
+        var confirm = window.confirm('You will be redirected to HostGator. ' +
+            'Once you have purchased hosting please return here to continue.');
+        if(confirm) {
+            $window.open(url);
+            $scope.model.hostingPurchased = true;
+        }
+    }
+}]);
+
+app.controller('namecheapCtrl', ['$scope', '$window', function ($scope, $window) {
+    $scope.model = {
+        domainPurchased: false,
+        domain: ''
+    };
+
+    $scope.buyDomain = function() {
+        if($scope.model.domain == '') {
+           alert('Please enter a valid domain name. Ex: domainname.com');
+            return;
+        }
+
+        $scope.model.domain = $scope.model.domain.replace('https://', '');
+        $scope.model.domain = $scope.model.domain.replace('http://', '');
+        $scope.model.domain = $scope.model.domain.replace('www.', '');
+
+        var url = 'https://www.namecheap.com/domains/registration/results.aspx?domain=' + $scope.model.domain;
+
+        var confirm = window.confirm('You will be redirected to NameCheap. ' +
+            'Once you have purchased your domain please return here to continue.');
+        if(confirm) {
+            $window.open(url);
+            $scope.model.domainPurchased = true;
+        }
+    }
 }]);
 
 app.config(['$routeProvider',
@@ -22,6 +79,10 @@ app.config(['$routeProvider',
             when('/hosting', {
                 templateUrl: 'hosting.html',
                 controller: 'hostingCtrl'
+            }).
+            when('/namecheap', {
+                templateUrl: 'namecheap.html',
+                controller: 'namecheapCtrl'
             }).
             when('/wordpress', {
                 templateUrl: 'wordpress.html',

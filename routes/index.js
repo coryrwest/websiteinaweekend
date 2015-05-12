161c4aa23dd6e01a9366d64940d0083599e21c37
Cory Westropp <cory.r.west@gmail.com>
2015-05-12 12:52:58 -0700

    Fixed routing for new structure.

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-    res.sendfile('./public/index.html');
-});
+//router.get('/', function(req, res) {
+//    res.sendfile('./public/index.html');
+//});
 
-router.get('/guide', function(req, res) {
+router.get('/', function(req, res) {
     res.sendfile('./public/guide.html');
 });
 

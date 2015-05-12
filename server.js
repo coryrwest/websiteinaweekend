d03f7476dda0b8065dd5d96912ad7a33feb4450e
Cory Westropp <cory.r.west@gmail.com>
2015-05-12 12:43:10 -0700

    Modified startup to work with openshift hopefully.

--- server.js
+++ server.js
@@ -69,7 +69,20 @@ module.exports = app;
 
 /* istanbul ignore next */
 if (!module.parent) {
-    var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-    app.listen(port);
+    //  Set the environment variables we need.
+    var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+    var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+    if (typeof ipaddress === "undefined") {
+        //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+        //  allows us to run/test the app locally.
+        console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+        ipaddress = "127.0.0.1";
+    }
+
+    app.listen(port, ipaddress, function() {
+        console.log('%s: Node server started on %s:%d ...',
+            Date(Date.now() ), self.ipaddress, self.port);
+    });
     console.log('Express started on port ' + port);
 }
